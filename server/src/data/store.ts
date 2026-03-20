0105776cf9b1426eb9ccf47c9eb4b4ea319cacd5
blobgoat <jacobaaronseaman@gmail.com>
2026-03-19 21:01:42 -0700

    test for branch push blocking

--- server/src/data/store.ts
+++ server/src/data/store.ts
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { type Client, type InventoryItem } from '../../../datastructs/global_types';
 
-
+//testing if i can push to main
 type Intake = {
   id: string;
   clientId: string;
