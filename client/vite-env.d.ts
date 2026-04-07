193dd41079f1eb56a4b3d3b5b4839f70cc6a6aa1
blobgoat <jacobaaronseaman@gmail.com>
2026-04-06 19:05:20 -0700

    added some moe supported files

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -7,3 +7,15 @@ declare module '*.css' {
     const content: string;
     export default content;
 }
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+declare module '*.jpeg' {
+    const value: string;
+    export default value;
+}
